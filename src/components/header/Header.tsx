7404ca48cfaded38e8ce4aff118735d74dd2a0d0
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-10-26 17:05:53 +0300

    have created component Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,29 @@
 import './Header.scss';
 
 export const Header = () => {
-  return <section className="header">Navbar content</section>;
+  return (
+    <section className="header">
+      <div className="money">
+        <div>
+          <span>bitcoin</span>
+          <span>13221654</span>
+        </div>
+        <div>
+          <span>bitcoin</span>
+          <span>13221654</span>
+        </div>
+        <div>
+          <span>bitcoin</span>
+          <span>13221654</span>
+        </div>
+      </div>
+      <div className="profile">
+        <h4>profile@gmail.com</h4>
+        <div className="avatar">
+          <img src="../../icons/money/bitcoin-btc-logo.svg" alt="" />
+        </div>
+        <div className="targets">134,32 USD +2,38 (1,80 %)</div>
+      </div>
+    </section>
+  );
 };
